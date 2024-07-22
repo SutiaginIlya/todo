b9777be2ad051a2226335bfcaaa359e3d0266821
Aleksei <SchekinAA@corp.tatneft.tatar>
2024-07-22 16:40:29 +0400

    fix: delete default export and fix module styles

--- todo-list/src/components/Main/Main.jsx
+++ todo-list/src/components/Main/Main.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Input from "../HeadInput/HeadInput";
-import List from "../List/List";
+import { List } from "../List/index";
 
 function Main() {
   const [tasks, setTasks] = useState([]);
