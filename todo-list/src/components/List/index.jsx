d8da0cd032aac823850255de0f272782a6ebe9b4
Сутягин Илья <ilya.sutiagin2012@yandex.ru>
2024-07-23 17:18:41 +0400

    fix: add kids components

--- todo-list/src/components/List/index.jsx
+++ todo-list/src/components/List/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./index.module.css";
+import { Buttons } from "../Buttons/index";
 
 export const List = ({ tasks, deleteTask, moveTaskUp, moveTaskDown }) => {
   return (
@@ -7,16 +8,13 @@ export const List = ({ tasks, deleteTask, moveTaskUp, moveTaskDown }) => {
       {tasks.map((task, index) => (
         <li className="animate__bounceIn" key={index}>
           <span className="text">{task}</span>
-          <button className="delete-btn" onClick={() => deleteTask(index)}>
-            Delete
-          </button>
-          <button className="delete-btn" onClick={() => moveTaskUp(index)}>
-            Up
-          </button>
-          <button className="delete-btn" onClick={() => moveTaskDown(index)}>
-            Down
-          </button>
-          <span className={styles.test_color}>test text</span>
+          <Buttons
+            index={index}
+            deleteTask={deleteTask}
+            moveTaskUp={moveTaskUp}
+            moveTaskDown={moveTaskDown}
+          />
+          {/* <span className={styles.test_color}>test text</span> */}
         </li>
       ))}
     </ol>
