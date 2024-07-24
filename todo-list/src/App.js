a312640059ab926380f9c969c2886e46519c7859
Сутягин Илья <ilya.sutiagin2012@yandex.ru>
2024-07-24 14:56:18 +0400

    fix: update css and add background pickture

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,8 +1,10 @@
 import ToDoList from "./ToDoList";
+import './App.css';
 
 export const App = () => {
   return (
     <div>
+      <div className="background"></div>
       <ToDoList />
     </div>
   );
--- todo-list/src/components/Buttons/index.jsx
+++ todo-list/src/components/Buttons/index.jsx
@@ -1,30 +1,27 @@
 import React from "react";
+import Stack from '@mui/material/Stack';
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import DeleteIcon from "@mui/icons-material/Delete";
-import Fab from "@mui/material/Fab";
-import EditIcon from "@mui/icons-material/Edit";
-import styles from "./index.module.css";
 
 export const Buttons = ({ index, deleteTask, moveTaskUp, moveTaskDown }) => {
   return (
-    <span className="button-box">
-      <IconButton
-        aria-label="delete"
-        color="error"
-        onClick={() => deleteTask(index)}
-      >
-        <DeleteIcon />
-      </IconButton>
-      <Button  onClick={() => moveTaskUp(index)}>
-        Up
-      </Button>
-      <Button  onClick={() => moveTaskDown(index)}>
-        Down
-      </Button>
-      <Fab color="primary" aria-label="edit" size="small">
-        <EditIcon size="large"/>
-      </Fab>
+    <span>
+      <Stack spacing={2} direction="row">
+        <IconButton
+          aria-label="delete"
+          color="error"
+          onClick={() => deleteTask(index)}
+        >
+          <DeleteIcon />
+        </IconButton>
+        <Button variant="contained" onClick={() => moveTaskUp(index)}>
+          Up
+        </Button>
+        <Button variant="contained" onClick={() => moveTaskDown(index)}>
+          Down
+        </Button>
+      </Stack>
     </span>
   );
 };
